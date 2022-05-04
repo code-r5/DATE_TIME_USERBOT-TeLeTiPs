55f8e7c4f2cac3b06eb69a0e50ed2b6bebdd6211
code-r5 <96444394+code-r5@users.noreply.github.com>
2022-05-04 21:01:58 +0300

    Update emojis_teletips.py

--- lists_teletips/emojis_teletips.py
+++ lists_teletips/emojis_teletips.py
@@ -1,1 +1,1 @@
-emojis_teletips = ['⭐','✨']
+emojis_teletips = ['⚠️ Warning: Many users reported this account as a pro or a legend😎😎😂','⚠️ Warning: Many users reported this account as a pro or a legend😎😎😂']
